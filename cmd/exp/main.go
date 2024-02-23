a193a8328be6774c87cfe03da12d1d93f11b22d6
Rapin <rapin.pakdee@gmail.com>
2024-02-23 22:11:46 +0700

    update with contexual-encoding

---
+++ cmd/exp/main.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"html/template"
+	"os"
+)
+
+type User struct {
+	Name string
+	Bio  string
+	Age  int
+}
+
+type UserMeta struct {
+	Visits int
+}
+
+func main() {
+	t, err := template.ParseFiles("hello.gohtml")
+	if err != nil {
+		panic(err)
+	}
+
+	user := User{
+		Name: "Jon",
+		Bio:  `<script>alert("Haha, you have been h4x0r3d!");</script>`,
+		Age:  123,
+	}
+
+	err = t.Execute(os.Stdout, user)
+
+	if err != nil {
+
+		panic(err)
+	}
+}
